bc0e161839931e0b7a46cdc4deb7320dccb52efc
xfangfang <2553041586@qq.com>
2022-11-15 09:26:23 +0800

    Fix syntax

--- Formula/ffmpeg-wiliwili.rb
+++ Formula/ffmpeg-wiliwili.rb
@@ -3,80 +3,80 @@
 # https://github.com/Homebrew/homebrew-core/blob/master/Formula/ffmpeg.rb
 
 class FfmpegWiliwili < Formula
-    desc "Play, record, convert, and stream audio and video"
-    homepage "https://ffmpeg.org/"
-    url "https://ffmpeg.org/releases/ffmpeg-5.1.2.tar.gz"
-    sha256 "87fe8defa37ce5f7449e36047171fed5e4c3f4bb73eaccea8c954ee81393581c"
-  
-    keg_only <<EOS
+  desc "Play, record, convert, and stream audio and video"
+  homepage "https://ffmpeg.org/"
+  url "https://ffmpeg.org/releases/ffmpeg-5.1.2.tar.gz"
+  sha256 "87fe8defa37ce5f7449e36047171fed5e4c3f4bb73eaccea8c954ee81393581c"
+
+  keg_only <<EOS
 it is intended to only be used for building wiliwili.
 This formula is not recommended for daily use and has no binaraies (ffmpeg, ffplay etc.)
 EOS
-  
-    on_intel do
-      depends_on "nasm" => :build
-    end
-    on_linux do
-        depends_on "alsa-lib"
-        depends_on "libxv"
-    end
-    depends_on "pkg-config" => :build
-    depends_on "dav1d"
-    depends_on "gnutls"
-    depends_on "freetype"
-    depends_on "fontconfig"
-    depends_on "libass"
-    uses_from_macos "bzip2"
-    uses_from_macos "libxml2"
-    uses_from_macos "zlib"
-  
-    fails_with gcc: "5"
-    
-    def install
-  
-      args = %W[
-        --prefix=#{prefix}
-        --enable-shared
-        --enable-pthreads
-        --enable-version3
-        --cc=#{ENV.cc}
-        --host-cflags=#{ENV.cflags}
-        --host-ldflags=#{ENV.ldflags}
-        --enable-gpl
-        --enable-gnutls
-        --enable-libxml2
-        --disable-autodetect
-        --disable-libjack
-        --disable-indev=jack
-        --disable-programs
-        --disable-videotoolbox
-        --disable-avdevice
-        --disable-postproc
-        --disable-doc
-        --disable-debug
-        --enable-network
-        --enable-libdav1d
-        --enable-zlib
-        --enable-bzlib
-	    --enable-libass
-        --enable-libfreetype
-	    --enable-libfontconfig
-        --disable-protocols
-	    --enable-protocol='file,http,tcp,udp,rtmp,hls,https,tls'
-        --disable-encoders
-      ]
 
-      args << "--enable-neon" if Hardware::CPU.arm?
-  
-      system "./configure", *args
-      system "make", "install"
-
-    end
-  
-    test do
-      # Create an example mp4 file
-      mp4out = testpath/"video.mp4"
-      system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=1", mp4out
-      assert_predicate mp4out, :exist?
-    end
+  on_intel do
+    depends_on "nasm" => :build
   end
+  depends_on "pkg-config" => :build
+  depends_on "dav1d"
+  depends_on "freetype"
+  depends_on "fontconfig"
+  depends_on "gnutls"
+  depends_on "libass"
+  uses_from_macos "bzip2"
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "alsa-lib"
+    depends_on "libxv"
+  end
+
+  fails_with gcc: "5"
+
+  def install
+
+    args = %W[
+      --prefix=#{prefix}
+      --enable-shared
+      --enable-pthreads
+      --enable-version3
+      --cc=#{ENV.cc}
+      --host-cflags=#{ENV.cflags}
+      --host-ldflags=#{ENV.ldflags}
+      --enable-gpl
+      --enable-gnutls
+      --enable-libxml2
+      --disable-autodetect
+      --disable-libjack
+      --disable-indev=jack
+      --disable-programs
+      --disable-videotoolbox
+      --disable-avdevice
+      --disable-postproc
+      --disable-doc
+      --disable-debug
+      --enable-network
+      --enable-libdav1d
+      --enable-zlib
+      --enable-bzlib
+      --enable-libass
+      --enable-libfreetype
+      --enable-libfontconfig
+      --disable-protocols
+      --enable-protocol='file,http,tcp,udp,rtmp,hls,https,tls'
+      --disable-encoders
+    ]
+
+    args << "--enable-neon" if Hardware::CPU.arm?
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    # Create an example mp4 file
+    mp4out = testpath/"video.mp4"
+    system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=1", mp4out
+    assert_predicate mp4out, :exist?
+  end
+end
--- Formula/mpv-wiliwili.rb
+++ Formula/mpv-wiliwili.rb
@@ -3,61 +3,61 @@
 # https://github.com/Homebrew/homebrew-core/blob/master/Formula/mpv.rb
 
 class MpvWiliwili < Formula
-    desc "Media player based on MPlayer and mplayer2"
-    homepage "https://mpv.io"
-    url "https://github.com/mpv-player/mpv/archive/v0.35.0.tar.gz"
-    sha256 "dc411c899a64548250c142bf1fa1aa7528f1b4398a24c86b816093999049ec00"
-  
-    keg_only "it is intended to only be used for building wiliwili. This formula is not recommended for daily use"
-  
-    depends_on "docutils" => :build
-    depends_on "pkg-config" => :build
-    depends_on "python@3.10" => :build
-    depends_on xcode: :build
-  
-    depends_on "ffmpeg-wiliwili"
-    depends_on "libass"
+  desc "Media player based on MPlayer and mplayer2"
+  homepage "https://mpv.io"
+  url "https://github.com/mpv-player/mpv/archive/v0.35.0.tar.gz"
+  sha256 "dc411c899a64548250c142bf1fa1aa7528f1b4398a24c86b816093999049ec00"
 
-    on_linux do
-        depends_on "alsa-lib"
-    end
-  
-    fails_with gcc: "5" # ffmpeg is compiled with GCC
+  keg_only "it is intended to only be used for building wiliwili. This formula is not recommended for daily use"
 
-    def install
-      # LANG is unset by default on macOS and causes issues when calling getlocale
-      # or getdefaultlocale in docutils. Force the default c/posix locale since
-      # that's good enough for building the manpage.
-      ENV["LC_ALL"] = "C"
+  depends_on "docutils" => :build
+  depends_on "pkg-config" => :build
+  depends_on "python@3.10" => :build
+  depends_on xcode: :build
 
-      # Avoid unreliable macOS SDK version detection
-      # See https://github.com/mpv-player/mpv/pull/8939
-      if OS.mac?
-        sdk = (MacOS.version == :big_sur) ? MacOS::Xcode.sdk : MacOS.sdk
-        ENV["MACOS_SDK"] = sdk.path
-        ENV["MACOS_SDK_VERSION"] = "#{sdk.version}.0"
-      end
-  
-      args = %W[
-        --prefix=#{prefix}
-        --disable-debug-build
-        --enable-libmpv-shared
-        --disable-jpeg
-        --disable-libavdevice
-        --confdir=#{etc}/mpv
-        --datadir=#{pkgshare}
-        --mandir=#{man}
-        --docdir=#{doc}
-        --zshdir=#{zsh_completion}
-      ]
-  
-      python3 = "python3.10"
-      system python3, "bootstrap.py"
-      system python3, "waf", "configure", *args
-      system python3, "waf", "install"
-    end
-  
-    test do
-      system bin/"mpv", "--ao=null", test_fixtures("test.wav")
-    end
+  depends_on "ffmpeg-wiliwili"
+  depends_on "libass"
+
+  on_linux do
+    depends_on "alsa-lib"
   end
+
+  fails_with gcc: "5" # ffmpeg is compiled with GCC
+
+  def install
+    # LANG is unset by default on macOS and causes issues when calling getlocale
+    # or getdefaultlocale in docutils. Force the default c/posix locale since
+    # that's good enough for building the manpage.
+    ENV["LC_ALL"] = "C"
+
+    # Avoid unreliable macOS SDK version detection
+    # See https://github.com/mpv-player/mpv/pull/8939
+    if OS.mac?
+      sdk = (MacOS.version == :big_sur) ? MacOS::Xcode.sdk : MacOS.sdk
+      ENV["MACOS_SDK"] = sdk.path
+      ENV["MACOS_SDK_VERSION"] = "#{sdk.version}.0"
+    end
+
+    args = %W[
+      --prefix=#{prefix}
+      --disable-debug-build
+      --enable-libmpv-shared
+      --disable-jpeg
+      --disable-libavdevice
+      --confdir=#{etc}/mpv
+      --datadir=#{pkgshare}
+      --mandir=#{man}
+      --docdir=#{doc}
+      --zshdir=#{zsh_completion}
+    ]
+
+    python3 = "python3.10"
+    system python3, "bootstrap.py"
+    system python3, "waf", "configure", *args
+    system python3, "waf", "install"
+  end
+
+  test do
+    system bin/"mpv", "--ao=null", test_fixtures("test.wav")
+  end
+end
