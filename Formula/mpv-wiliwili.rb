7d1c1dbd4dc13ce02fc81831569c8a9c424abc75
xfangfang <2553041586@qq.com>
2023-03-28 10:52:27 +0800

    mpv-wiliwili: revision
    * Using meson instead of waf
    * Fix build error
    
    Closes #12.
    
    Signed-off-by: xfangfang <16631584+xfangfang@users.noreply.github.com>

--- Formula/mpv-wiliwili.rb
+++ Formula/mpv-wiliwili.rb
@@ -7,6 +7,7 @@ class MpvWiliwili < Formula
   homepage "https://mpv.io"
   url "https://github.com/mpv-player/mpv/archive/v0.35.1.tar.gz"
   sha256 "41df981b7b84e33a2ef4478aaf81d6f4f5c8b9cd2c0d337ac142fc20b387d1a9"
+  revision 1
 
   bottle do
     root_url "https://github.com/xfangfang/homebrew-wiliwili/releases/download/mpv-wiliwili-0.35.1"
@@ -18,8 +19,8 @@ class MpvWiliwili < Formula
   keg_only "it is intended to only be used for building wiliwili 1"
 
   depends_on "docutils" => :build
+  depends_on "meson" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@3.10" => :build
   depends_on xcode: :build
 
   depends_on "ffmpeg-wiliwili"
@@ -30,43 +31,35 @@ class MpvWiliwili < Formula
     depends_on "alsa-lib"
   end
 
-  fails_with gcc: "5" # ffmpeg is compiled with GCC
-
   def install
     # LANG is unset by default on macOS and causes issues when calling getlocale
     # or getdefaultlocale in docutils. Force the default c/posix locale since
     # that's good enough for building the manpage.
     ENV["LC_ALL"] = "C"
 
-    # Avoid unreliable macOS SDK version detection
-    if OS.mac?
-      sdk = (MacOS.version == :big_sur) ? MacOS::Xcode.sdk : MacOS.sdk
-      ENV["MACOS_SDK"] = sdk.path
-      ENV["MACOS_SDK_VERSION"] = "#{sdk.version}.0"
-    end
+    # force meson find ninja from homebrew
+    ENV["NINJA"] = Formula["ninja"].opt_bin/"ninja"
 
     args = %W[
-      --prefix=#{prefix}
-      --disable-debug-build
-      --enable-libmpv-shared
-      --disable-jpeg
-      --disable-libavdevice
-      --disable-swift
-      --disable-javascript
-      --disable-macos-touchbar
-      --disable-macos-media-player
-      --disable-cplayer
-      --confdir=#{etc}/mpv
+      -Dlibmpv=true
+      -Dcplayer=false
+      -Dlua=luajit
+
+      -Dswift-build=disabled
+      -Dmacos-cocoa-cb=disabled
+      -Dmacos-media-player=disabled
+      -Dmacos-touchbar=disabled
+
+      -Dmanpage-build=disabled
+      -Dhtml-build=disabled
+
+      --sysconfdir=#{pkgetc}
       --datadir=#{pkgshare}
-      --mandir=#{man}
-      --docdir=#{doc}
-      --zshdir=#{zsh_completion}
     ]
 
-    python3 = "python3.10"
-    system python3, "bootstrap.py"
-    system python3, "waf", "configure", *args
-    system python3, "waf", "install"
+    system "meson", "setup", "build", *args, *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
   end
 
   test do
