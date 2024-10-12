0d2d79553b748e01b512ace85ea5ba1e9dd2661c
xfangfang <2553041586@qq.com>
2024-10-12 14:24:41 +0800

    fresh start
    Closes #36.
    
    Signed-off-by: xfangfang <16631584+xfangfang@users.noreply.github.com>

--- Formula/mpv-wiliwili.rb
+++ Formula/mpv-wiliwili.rb
@@ -8,14 +8,6 @@ class MpvWiliwili < Formula
   url "https://github.com/mpv-player/mpv/archive/refs/tags/v0.39.0.tar.gz"
   sha256 "2ca92437affb62c2b559b4419ea4785c70d023590500e8a52e95ea3ab4554683"
 
-  bottle do
-    root_url "https://github.com/xfangfang/homebrew-wiliwili/releases/download/mpv-wiliwili-0.39.0"
-    rebuild 1
-    sha256 cellar: :any, arm64_sequoia: "46f8d04b0e670264e1078fa3bfd8228bfa806d16375948ad1ec011e242452976"
-    sha256 cellar: :any, arm64_sonoma:  "9d03bc0345f3809d0f013074c2ae26c4630a6de3f023dcbfd0e86e7ea02d2fbc"
-    sha256 cellar: :any, ventura:       "aef0ceabbb1688e2dead86f4cc59ad570d707ade6d979ff22864de584a6bf1dd"
-  end
-
   keg_only "it is intended to only be used for building wiliwili"
 
   depends_on "docutils" => :build
