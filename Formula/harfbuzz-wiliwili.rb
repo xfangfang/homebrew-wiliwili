ef7a4006a9dcf8c3e0aad403575b26f45fac08e7
xfangfang <16631584+xfangfang@users.noreply.github.com>
2024-10-12 04:14:09 +0000

    harfbuzz-wiliwili: add 10.0.1 bottle.

--- Formula/harfbuzz-wiliwili.rb
+++ Formula/harfbuzz-wiliwili.rb
@@ -7,6 +7,13 @@ class HarfbuzzWiliwili < Formula
 
   head "https://github.com/harfbuzz/harfbuzz.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/xfangfang/homebrew-wiliwili/releases/download/harfbuzz-wiliwili-10.0.1"
+    sha256 cellar: :any, arm64_sequoia: "a33e52ee43bb0bc990f98eeedf4dbdb53aa321bc6a2eb7cb0be4bdf74bd2cf91"
+    sha256 cellar: :any, arm64_sonoma:  "24e29667b2e9ac8dd1eed51d3fab61e4706cba8944a26c7db7e51f086b968446"
+    sha256 cellar: :any, ventura:       "f8b63961e09077f2506f05ef2048af28391c4e6d00b930f62b24dd42c7018774"
+  end
+
   depends_on "gobject-introspection" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
